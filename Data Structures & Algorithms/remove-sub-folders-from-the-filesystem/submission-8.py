d29e44063961fd79b5b969b35ca973b807823b93
Alex Dong <81170256+alexanderdong8@users.noreply.github.com>
2026-06-30 14:42:18 -0400

    Add: remove-sub-folders-from-the-filesystem - submission-8

---
+++ Data Structures & Algorithms/remove-sub-folders-from-the-filesystem/submission-8.py
@@ -0,0 +1,45 @@
+class Trie:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        # folder.sort()
+        root = Trie()
+        res = []
+        seen = set()
+        for path in folder:
+            pathArr = path.split("/") # remember if it has it in the beginning it will return nothing
+            pathArr = pathArr[1:]
+            curr = root
+            newPath = True
+            for subPath in pathArr:
+                print(subPath)
+                if subPath not in curr.children:
+                    curr.children[subPath] = Trie()
+                # else:
+                #     # if curr.children[subPath].end:
+                #     #     print(subPath, curr.children[subPath].end)
+                #     #     newPath = False
+                curr = curr.children[subPath]
+
+            curr.end = True
+
+        arr = ["/"]
+        def dfs(node):
+            if node.end:
+                res.append("".join(arr[:-1]))
+                return
+            
+            for newNode in node.children:
+                arr.append(newNode)
+                arr.append("/")
+                dfs(node.children[newNode])
+                arr.pop()
+                arr.pop()
+            
+            return
+        
+        dfs(root)
+
+        return res
