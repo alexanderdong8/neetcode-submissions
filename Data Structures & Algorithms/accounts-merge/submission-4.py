b0908b1ef66f4e4771bf649aae4da2cc2a248ef3
Alex Dong <81170256+alexanderdong8@users.noreply.github.com>
2026-08-11 23:04:43 -0400

    Add: accounts-merge - submission-4

---
+++ Data Structures & Algorithms/accounts-merge/submission-4.py
@@ -0,0 +1,49 @@
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        '''
+        map from name to emails
+        map from emails to name 
+        email to name 
+        map from index to name 
+        index to emails
+        emails to index
+        iterate through the 
+        '''
+        index_to_email = defaultdict(list)
+        index_to_name = {}
+        email_to_index = defaultdict(list)
+
+        for index, account in enumerate(accounts):
+            index_to_name[index] = account[0]
+            for x in range(1, len(account)):
+                index_to_email[index].append(account[x])
+                email_to_index[account[x]].append(index)
+
+        res = []
+        seen = set()
+        seen_id = set()
+
+        def dfs(index):
+            print(index)
+            for email in index_to_email[index]:
+                print(email, index)
+                if email not in seen:
+                    seen.add(email)
+                    arr.append(email)
+                    for index in email_to_index[email]:
+                        dfs(index)
+
+
+
+        for key, value in index_to_email.items():
+            arr = []
+            print(key, value, "start")
+            seen_id.add(key)
+            dfs(key)
+            if len(arr) == 0:
+                continue
+            arr.sort()
+            arr.insert(0, index_to_name[key])
+            res.append(arr)
+
+        return res
